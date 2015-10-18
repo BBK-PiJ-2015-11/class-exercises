a3df0ca114f1d45dc173244a6d60bbd06629306f
Matt Bostock <matt@mattbostock.com>
2015-10-18 13:58:10 +0100

    Add day 3 exercise 1
    The original code fails to equate `d1` and `d2` because of floating
    point imprecision.
    
    By checking for a small difference between the two numbers, we can
    assert approximate equality.

---
+++ day_3/1.groovy
@@ -0,0 +1,9 @@
+double d1 = 1.255
+double d2 = d1 + 7 - 7
+
+// see day 3 class notes, section 2.2.2 'Equality'
+if (Math.abs(d1 - d2) < 10E-6) {
+  println("1.255 is equal to 1.255 plus 7 minus 7");
+} else {
+  println("1.255 is NOT equal to 1.255 plus 7 minus 7");
+}
