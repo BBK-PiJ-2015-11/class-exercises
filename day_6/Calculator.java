ea98692c4c52aed2368ddb56b1909a9e9e6dc2d8
Matt Bostock <matt@mattbostock.com>
2015-11-02 18:59:57 +0000

    Add day 6 exercise 1
    All methods in the Calculator class are exported publicly so that they
    can be used from the Exercise1 class.

---
+++ day_6/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+  public int add(int n, int m) {
+    return n + m;
+  }
+
+  public int subtract(int n, int m) {
+    return n - m;
+  }
+
+  public int multiply(int n, int m) {
+    return n * m;
+  }
+
+  public double divide(int n, int m) {
+    return (double) n / (double) m;
+  }
+
+  public int modulus(int n, int m) {
+    return n % m;
+  }
+
+}
---
+++ day_6/Exercise1.java
@@ -0,0 +1,13 @@
+public class Exercise1 {
+
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+
+    System.out.println(calculator.add(4,5));
+    System.out.println(calculator.subtract(4,5));
+    System.out.println(calculator.multiply(4,5));
+    System.out.println(calculator.divide(4,5));
+    System.out.println(calculator.modulus(4,5));
+  }
+
+}
