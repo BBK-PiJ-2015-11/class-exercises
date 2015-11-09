782b0290653466f1b7ad1508a94ea8502bde8bd5
Matt Bostock <matt@mattbostock.com>
2015-11-09 19:33:59 +0000

    Add day 7 exercise 3

---
+++ day_7/exercise_3/Patient.java
@@ -0,0 +1,40 @@
+public class Patient {
+	public String name;
+	public int age;
+	public String illness;
+	public Patient nextPatient;
+	
+	public Patient(String name, int age, String illness) {
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.nextPatient = null;
+	}
+
+	public void addPatient(Patient newPatient) {
+		if (this.nextPatient == null) {
+			this.nextPatient = newPatient;
+		} else {
+			this.nextPatient.addPatient(newPatient);
+		}
+	}
+	
+	public boolean deletePatient(Patient patient) {
+		if (this.nextPatient == null) {
+			return false;
+		} else if (this.nextPatient.name.equals(patient.name)) {
+			// FIXME: Assumes patient name is unique
+			this.nextPatient = nextPatient.nextPatient;
+			return true;
+		} else {
+			return this.nextPatient.deletePatient(patient);
+		}
+	}
+	
+	public void printAll() {
+		System.out.println(this.name);
+		if (this.nextPatient != null) {
+			this.nextPatient.printAll();
+		}
+	}
+}
