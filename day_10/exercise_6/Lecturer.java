e75fc53a66489e75da259db241abb720cb82f117
Matt Bostock <matt@mattbostock.com>
2015-11-30 19:38:26 +0000

    Add constructor method to Lecturer class
    Add a constructor method to the Lecturer class, which is necessary as
    its parent, Teacher, takes one argument for its constructor.

--- day_10/exercise_6/Lecturer.java
+++ day_10/exercise_6/Lecturer.java
@@ -2,6 +2,10 @@
  * A lecturer has both teaching and research responsibilities
  */
 public class Lecturer extends Teacher {
+  public Lecturer(String name) {
+    super(name);
+  }
+
   public void doResearch(String topic) {
     System.out.println("Doing research on: " + topic);
   }
